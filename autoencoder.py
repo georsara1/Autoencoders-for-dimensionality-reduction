6002a620aea7c980345ba56216a4da94b6cc6b88
georsara1 <georsara11@gmail.com>
2018-03-12 23:22:45 +0200

    Use of AutoEncoders for dimensinality reduction
    This repo exhibits the use of AutoEncoders, a special class of Neural
    Network of varying Architectures, to perform dimensionality reduction in
    the feature space

---
+++ autoencoder.py
@@ -0,0 +1,176 @@
+
+#----------------------------------Import modules------------------------------
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential, Model
+from keras.layers import Activation, Dense, Dropout, Input
+from keras import optimizers, regularizers
+from sklearn.metrics import confusion_matrix,accuracy_score, roc_curve, auc
+from sklearn import preprocessing
+sns.set_style("whitegrid")
+
+np.random.seed(697)
+
+#Import data
+df = pd.read_excel('dataset.xls', header = 1)
+df = df.rename(columns = {'default payment next month': 'Default'})
+
+#---------------------------------Pre-processing--------------------------------
+#Check for missing values
+df.isnull().sum() #No missing values thus no imputations needed
+
+#Drop unneeded variables
+df = df.drop(['ID'], axis = 1)
+
+#Encode categorical variables to ONE-HOT
+print('Converting categorical variables to numeric...')
+
+categorical_columns = ['SEX', 'EDUCATION', 'MARRIAGE', 'PAY_0', 'PAY_2', 'PAY_3', 'PAY_4', 'PAY_5', 'PAY_6']
+
+df = pd.get_dummies(df, columns = categorical_columns)
+
+#Scale variables to [0,1] range
+columns_to_scale = ['LIMIT_BAL', 'AGE', 'BILL_AMT1', 'BILL_AMT2', 'BILL_AMT3', 'BILL_AMT4', 'BILL_AMT5'
+    , 'BILL_AMT6', 'PAY_AMT1', 'PAY_AMT2', 'PAY_AMT3', 'PAY_AMT4', 'PAY_AMT5', 'PAY_AMT6']
+
+df[columns_to_scale]=df[columns_to_scale].apply(lambda x: (x-x.min())/(x.max()-x.min()))
+
+#Split in 75% train and 25% test set
+train, test_df = train_test_split(df, test_size = 0.15, random_state= 1984)
+train_df, dev_df = train_test_split(train, test_size = 0.15, random_state= 1984)
+
+# Check distribution of labels in train and test set
+train_df.Default.sum()/train_df.shape[0] #0.2210
+dev_df.Default.sum()/dev_df.shape[0] #0.2269
+test_df.Default.sum()/test_df.shape[0] #0.2168
+
+# Define the final train and test sets
+train_y = train_df.Default
+dev_y = dev_df.Default
+test_y = test_df.Default
+
+train_x = train_df.drop(['Default'], axis = 1)
+dev_x = dev_df.drop(['Default'], axis = 1)
+test_x = test_df.drop(['Default'], axis = 1)
+
+train_x =np.array(train_x)
+dev_x =np.array(dev_x)
+test_x = np.array(test_x)
+
+train_y = np.array(train_y)
+dev_y = np.array(dev_y)
+test_y = np.array(test_y)
+
+#------------------------------------Build the AutoEncoder------------------------------------
+
+# Choose size of our encoded representations (we will reduce our initial features to this number)
+encoding_dim = 16
+
+# this is our input placeholder
+input_data = Input(shape=(train_x.shape[1],))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='elu')(input_data)
+# "decoded" is the reconstruction of the input
+decoded = Dense(train_x.shape[1], activation='sigmoid')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input_data, decoded)
+
+# this model maps an input to its encoded representation
+encoder = Model(input_data, encoded)
+
+# Create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# Retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# Create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+#Compile the autoencoder model
+autoencoder.compile(optimizer='adam',
+                    loss='binary_crossentropy')
+
+#Fit to train set, validate with dev set and save to hist_auto for plotting purposes
+hist_auto = autoencoder.fit(train_x, train_x,
+                epochs=50,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(dev_x, dev_x))
+
+# Summarize history for loss
+plt.figure()
+plt.plot(hist_auto.history['loss'])
+plt.plot(hist_auto.history['val_loss'])
+plt.title('Autoencoder model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper right')
+plt.show()
+
+# Encode and decode our test set (compare them vizually just to get a first insight of the autoencoder's performance)
+encoded_x = encoder.predict(test_x)
+decoded_output = decoder.predict(encoded_x)
+
+#--------------------------------Build new model using encoded test set--------------------------
+model = Sequential()
+model.add(Dense(16, input_dim=encoded_x.shape[1],
+                kernel_initializer='normal',
+                #kernel_regularizer=regularizers.l2(0.02),
+                activation="relu"
+                )
+          )
+model.add(Dropout(0.2))
+model.add(Dense(1))
+model.add(Activation("sigmoid"))
+model.compile(loss="binary_crossentropy", optimizer='adam')
+
+history = model.fit(encoded_x, test_y, validation_split=0.2, epochs=10, batch_size=64)
+
+# Summarize history for loss
+plt.figure()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Encoded model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper right')
+plt.show()
+
+#---------------------------------Predictions and visuallizations-----------------------
+#Predict on test set
+predictions_NN_prob = model.predict(encoded_x)
+predictions_NN_prob = predictions_NN_prob[:,0]
+
+predictions_NN_01 = np.where(predictions_NN_prob > 0.5, 1, 0) #Turn probability to 0-1 binary output
+
+#Print accuracy
+acc_NN = accuracy_score(test_y, predictions_NN_01)
+print('Overall accuracy of Neural Network model:', acc_NN)
+
+#Print Area Under Curve
+false_positive_rate, recall, thresholds = roc_curve(test_y, predictions_NN_prob)
+roc_auc = auc(false_positive_rate, recall)
+plt.figure()
+plt.title('Receiver Operating Characteristic (ROC)')
+plt.plot(false_positive_rate, recall, 'b', label = 'AUC = %0.3f' %roc_auc)
+plt.legend(loc='lower right')
+plt.plot([0,1], [0,1], 'r--')
+plt.xlim([0.0,1.0])
+plt.ylim([0.0,1.0])
+plt.ylabel('Recall')
+plt.xlabel('Fall-out (1-Specificity)')
+plt.show()
+
+#Print Confusion Matrix
+cm = confusion_matrix(test_y, predictions_NN_01)
+labels = ['No Default', 'Default']
+plt.figure(figsize=(8,6))
+sns.heatmap(cm,xticklabels=labels, yticklabels=labels, annot=True, fmt='d', cmap="Blues", vmin = 0.2);
+plt.title('Confusion Matrix')
+plt.ylabel('True Class')
+plt.xlabel('Predicted Class')
+plt.show()
+
